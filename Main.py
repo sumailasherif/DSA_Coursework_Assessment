96dc903fd37d95e4042ce7c3e45259e8b1cfd8d9
Sherif <sumailasherif773@gmail.com>
2026-09-13 23:15:40 +0400

    timer added

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
+from timeit import default_timer as timer
+
+from selection_sort import selection_sort
+from merge_sort import merge_sort
+from random_arrays import generate_random_array
 from test import run_correctness_checks
 from bench_mark_output import run_benchmark, print_growth_check, save_results
 from Results_plot import plot_comparison
 
-def main() -> None:
+def run_and_time(sort_func, arr: list, name: str)-> None:
     print("Step 1: Correctness checks")
     run_correctness_checks()
 
